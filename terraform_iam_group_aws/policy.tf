7503125e14e5635c98a7636a0f85125114da8b53
Najeeb <najeebkhot.1gmail.com>
2023-03-26 18:34:16 +0530

    Merged EC2 and IAM branch

---
+++ terraform_iam_group_aws/policy.tf
@@ -0,0 +1,22 @@
+#Creating IAM Admin Policy
+
+resource "aws_iam_policy" "admin-policy" {
+    name = "administrator"
+    policy = data.aws_iam_policy_document.sec-admin-permission.json
+}
+
+#This policy allows all IAM action to the user/group
+data "aws_iam_policy_document" "sec-admin-permission" {
+  statement {
+    effect = "Allow"
+    actions   = ["iam:*"]
+    resources = ["*"]
+  }
+}
+
+#Attaching the policy to the group/user
+resource "aws_iam_user_policy_attachment" "sec-admin-access" {
+    #user = aws_iam_user.admin_user.name
+    group      = aws_iam_group.security.name
+    policy_arn = aws_iam_policy.admin-policy.arn
+}
