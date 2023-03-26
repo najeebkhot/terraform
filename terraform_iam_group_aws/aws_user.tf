42605ca2e8e32647f7e4446cb02d0f9ac7b5c44c
Najeeb <najeebkhot.1gmail.com>
2023-03-26 17:05:51 +0530

    Added description header

--- terraform_iam_group_aws/aws_user.tf
+++ terraform_iam_group_aws/aws_user.tf
@@ -1,23 +1,26 @@
-provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
-
+#file for creating user and providing password as output
 
 resource "aws_iam_user" "admin_user" {
-    name = var.users.name
+    name = "admin"
     tags = {
         description = "admin user for creating other users"
     }
 }
 
-resource "aws_iam_policy" "adminuser" {
-    name = "administrator"
-    policy = data.aws_iam_policy_document.admin_admin.arn
+resource "aws_iam_user_login_profile" "login_password" {
+  user    = aws_iam_user.admin_user.name
 }
 
-data "aws_iam_policy_document" "admin_admin" {
+output "password" {
+  value = aws_iam_user_login_profile.login_password
+}
+
+resource "aws_iam_policy" "adminuser" {
+    name = "administrator"
+    policy = data.aws_iam_policy_document.sec-admin.json
+}
+
+data "aws_iam_policy_document" "sec-admin" {
   statement {
     effect = "Allow"
     actions   = ["iam:*"]
@@ -25,37 +28,7 @@ data "aws_iam_policy_document" "admin_admin" {
   }
 }
 
-resource "aws_iam_user_policy_attachment" "admin_admin-access" {
+resource "aws_iam_user_policy_attachment" "sec-admin-access" {
     user = aws_iam_user.admin_user.name
-    policy = aws_iam_policy.adminuser
-}
-
-resource "aws_iam_group" "security" {
-  name = "sec_admin"
-}
-
-resource "aws_iam_group_membership" "admin_access" {
-    name = "admin-access-group-membership"
-    users = aws_iam_user.admin_user.name
-    group = aws_iam_group.security.name
-}
-
-resource "aws_iam_group_policy" "admin_access_team_policy" {
-    name  = "my_developer_policy"
-    group = aws_iam_group.security.name
-
-    policy = jsonencode({
-        Statement = [
-        {
-            Action = ["iam:*"]
-            Effect   = "Allow"
-            Resource = "*"
-        },
-        ]
-    })
-}
-
-resource "aws_iam_group_policy_attachment" "security_attach" {
-  group      = aws_iam_group.security.name
-  policy_arn = aws_iam_policy.admin_access_team_policy.arn
+    policy_arn = aws_iam_policy.adminuser.arn
 }
