dc3bd9dd2825a0e016006acc18b3e28bdfb1831a
Najeeb <najeebkhot.1gmail.com>
2023-04-17 07:28:36 +0530

    Updated files for internet gateway and added file for creating nat gateway

--- terraform_infra_deployment/internet_gateway.tf
+++ terraform_infra_deployment/internet_gateway.tf
@@ -1,3 +1,8 @@
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
+
+#resource "aws_internet_gateway_attachment" "igw_vpc_association" {
+#  internet_gateway_id = aws_internet_gateway.igw.id
+#  vpc_id              = aws_vpc.vpc.id
+#}
